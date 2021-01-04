4c707978bfc42983e29b02f4d542a13f7b9d2a11
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-04 13:09:50 -0300

    POST /orders

--- backend/src/main/java/com/lucasmonteiro/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/lucasmonteiro/dsdeliver/services/OrderService.java
@@ -1,6 +1,7 @@
 // services --> É a camada que faz o meio de campo entre o controlador (que recebe a requisição) e a camada de acesso a dados que é o repository
 package com.lucasmonteiro.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lucasmonteiro.dsdeliver.dto.OrderDTO;
+import com.lucasmonteiro.dsdeliver.dto.ProductDTO;
 import com.lucasmonteiro.dsdeliver.entities.Order;
+import com.lucasmonteiro.dsdeliver.entities.OrderStatus;
+import com.lucasmonteiro.dsdeliver.entities.Product;
 import com.lucasmonteiro.dsdeliver.repositories.OrderRepository;
+import com.lucasmonteiro.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -18,11 +23,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrderWithProducts();
 		
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
